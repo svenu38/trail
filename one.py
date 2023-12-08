cb1868e6ef257f2572806787ec7956916e1281ff
VENU GOVINDARAJU <svenu38@yahoo.com>
2023-12-08 16:44:44 +0100

    added ipynb file

--- one.py
+++ one.py
@@ -22,3 +22,16 @@ for i in range(1,11):
     check_even_odd(i)
     
 print("the code is running perfet")
+
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+
+class dataset :
+    dataset = pd.read_csv("adult_reconstruction.csv")
+    print(dataset.head(5))
+    print(dataset.info)
+
+class distribution :
+    print()
